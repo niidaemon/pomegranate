eef92fd33f643c6b4468472b16580292a9c00150
niidaemon <samuelcodes4@gmail.com>
2025-10-03 23:39:32 -0400

    auth: setup

--- services/auth/src/main/java/com/pomegranate/auth/AuthApplication.java
+++ services/auth/src/main/java/com/pomegranate/auth/AuthApplication.java
@@ -1,4 +1,4 @@
-package com.pomegranate.services;
+package com.pomegranate.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
